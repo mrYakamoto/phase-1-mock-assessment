b84d1ed7567f233a6b457acca494c2bbc8d619a5
Jen Gilbert <j.h.gilbert@gmail.com>
2015-07-15 23:24:38 -0700

    Flesh out directions

--- guessing_game/README.md
+++ guessing_game/README.md
@@ -4,22 +4,24 @@ Ignore bonus.md for now -- that's a bonus release for when/if you've finished th
 
 ## Release 0
 
-Make all of the driver code in driver_code_release_0.rb evaluate to true. Running 
+Make all of the driver code in driver_code_release_0.rb evaluate to true. Running
 
     ruby driver_code_release_0.rb
-    
+
 should result in a bunch of "true"s printing to the screen.
 
-(Right now, it fails when it tries to run -- that's normal.)
+(Right now, it fails when it tries to run -- that's normal, since you haven't written anything yet!)
 
-Tip: This is driver code, not RSpec -- you must solve the tests in order because each line of driver code depends on the lines above it running correctly.
+Tip: This is driver code, not RSpec -- you must solve the tests in order, because each line of driver code depends on the lines above it running correctly.
 
-*Don't forget to commit after each release!*
+*Don't forget to commit!*
 
 ## Release 1
 
 Make all of the driver code in driver_code_release_1.rb evaluate to true.
 
-*Don't forget to commit after each release!*
+Note: This will break Release 0. That's okay, but commit before you start.
+
+*Don't forget to commit when you finish Release 1! From here on out, it's up to you to remember.*
 
 
--- guessing_game/driver_code_release_1.rb
+++ guessing_game/driver_code_release_1.rb
@@ -2,43 +2,45 @@ require_relative 'guessing_game'
 
 puts "We can initialize a guessing game with our desired 'secret number'"
 game = GuessingGame.new(8)
-p game.class == GuessingGame
+p (game.class == GuessingGame)
 
 puts "We can set a custom congrats message"
-p game.congrats_message == "Yay, you won!"
+p (game.congrats_message == "Yay, you won!")
 game.congrats_message = "You win!"
-p game.congrats_message == "You win!"
+p (game.congrats_message == "You win!")
 
 puts "There will be 5 guesses remaining"
-p game.remaining_guesses == 5
+p (game.remaining_guesses == 5)
 
 puts "User has neither won nor lost yet"
-p game.has_won? == false
-p game.has_lost? == false
+p (game.has_won? == false)
+p (game.has_lost? == false)
 
 puts "Wrong guess receives correct feedback"
-p game.guess(2) == "Too low!"
+p (game.guess(2) == "Too low!")
 
 puts "Wrong guess deducts from remaining guesses"
-p game.remaining_guesses == 4
+p (game.remaining_guesses == 4)
 
 puts "Repeated guesses do not cost the user anything"
-p game.guess(2) == "Too low!"
-p game.remaining_guesses == 4
+p (game.guess(2) == "Too low!")
+p (game.remaining_guesses == 4)
 
 puts "Wrong guess receives feedback"
-p game.guess(11) == "Too high!"
+p (game.guess(11) == "Too high!")
 
 puts "Wrong guess deducts from remaining guesses"
-p game.remaining_guesses == 3
+p (game.remaining_guesses == 3)
 
-p game.guess(10) == "Too high!"
-p game.remaining_guesses == 2
+p (game.guess(10) == "Too high!")
+p (game.remaining_guesses == 2)
 
 puts "Warning is given when there's only one guess remaining"
-p game.guess(9) == "Too high! WARNING: Only one guess left!"
-p game.guess(8) == "Correct! You win!"
+p (game.guess(9) == "Too high! WARNING: Only one guess left!")
+
+puts "Correct guess gets feedback and then congrats message"
+p (game.guess(8) == "Correct! You win!")
 
 puts "Outcome is recorded correctly"
-p game.has_won? == true
-p game.has_lost? == false
+p (game.has_won? == true)
+p (game.has_lost? == false)
