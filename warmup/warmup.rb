a31672c9129dcc26348d920ce8d7509e38a87650
mrYakamoto <wehmenstein@gmail.com>
2015-12-10 09:49:31 -0800

    finish warmup exercises

--- warmup/warmup.rb
+++ warmup/warmup.rb
@@ -1,8 +1,15 @@
-def say_bye(name)
+def say_bye(name = "")
+  return "Bye!" if name == ""
+  "Bye, #{name}!"
 end
 
 def sum_values(input_hash)
+  input_hash.values.reduce(:+)
 end
 
 def difference_two_biggest(arr)
+  return 0 if arr == nil || arr.empty?
+  return arr[0] if arr.length == 1
+  arr.sort
+  arr.pop() - arr.pop()
 end
