e62431cbb37e471a1f821c3f7f010edc7219bddc
mrYakamoto <wehmenstein@gmail.com>
2015-12-10 11:57:51 -0800

    add rspec for guessing_game

--- guessing_game/guessing_game.rb
+++ guessing_game/guessing_game.rb
@@ -2,62 +2,48 @@
 
 class GuessingGame
   attr_accessor :congrats_message, :remaining_guesses, :has_won, :has_lost
-  # @@game_number = 1
 
   def initialize(secret_number, num_allowed_guesses)
-    # @game_number = @@game_number
-    # @@game_number += 1
     @secret_number = secret_number
     @num_allowed_guesses = num_allowed_guesses
     @congrats_message = "Yay, you won!"
     @remaining_guesses = num_allowed_guesses
     @nums_guessed = []
-    @status = "playing"
+    @game_status = "active"
   end
 
-  def guess(number)
-    if @remaining_guesses == 0
-      "You already #{@status}. The number was #{@secret_number}"
-
-    elsif number == @secret_number
+  def guess(guessed_number)
+    return "You already #{@game_status}. The number was #{@secret_number}" if @remaining_guesses == 0
+    if guessed_number == @secret_number
       @remaining_guesses -= 1
-      @status = "won"
+      @game_status = "won"
       "Correct! The number was #{@secret_number}"
-    elsif number < @secret_number
-      if @nums_guessed.include?(number)
-        "Too low!"
-      else
-        @nums_guessed << number
-        @remaining_guesses -= 1
-        if @remaining_guesses == 1
-          "Too low! WARNING: Only one guess left!"
-        elsif @remaining_guesses == 0
-          @status = "lost"
-          "You lost! The number was #{@secret_number}"
-        else
-          "Too low!"
-        end
-      end
-    elsif number > @secret_number
-      if @nums_guessed.include?(number)
-        "Too high!"
-      else
-        @nums_guessed << number
-        @remaining_guesses -= 1
-        if @remaining_guesses == 1
-          "Too high! WARNING: Only one guess left!"
-        elsif @remaining_guesses == 0
-          @status = "lost"
-          "You lost! The number was #{@secret_number}"
-        else
-          "Too high!"
-        end
-      end
+    elsif guessed_number < @secret_number
+      apply_guess("low", guessed_number)
+    elsif guessed_number > @secret_number
+      apply_guess("high", guessed_number)
     end
   end
 
+  def apply_guess(relation, guessed_number)
+    if @nums_guessed.include?(guessed_number)
+        "Too #{relation}!"
+      else
+        @nums_guessed << guessed_number
+        @remaining_guesses -= 1
+        if @remaining_guesses == 1
+          "Too #{relation}! WARNING: Only one guess left!"
+        elsif @remaining_guesses == 0
+          @game_status = "lost"
+          "You lost! The number was #{@secret_number}"
+        else
+          "Too #{relation}!"
+        end
+      end
+  end
+
   def has_won?
-    if @status == "won"
+    if @game_status == "won"
       true
     else
       false
@@ -65,19 +51,11 @@ class GuessingGame
   end
 
   def has_lost?
-    if @status == "lost"
+    if @game_status == "lost"
       true
     else
       false
     end
   end
-
-  # def remaining_guesses
-  #   if @num_allowed_guesses == 1
-  #     puts "There will be 1 guess remaining in game #{@game_number}"
-  #   else
-  #     puts "There will be #{@num_allowed_guesses} guesses remaining in game #{@game_number}"
-  #   end
-  # end
 
 end
--- guessing_game/spec/guessing_game_rspec.rb
+++ guessing_game/spec/guessing_game_rspec.rb
@@ -11,4 +11,93 @@ describe "GuessingGame tests" do
     end
   end
 
+  describe "congrats message can be customized" do
+    it "alters congrats message on re-assignment" do
+      original = game.congrats_message
+      game.congrats_message = "Correct!"
+      expect(game.congrats_message).not_to eq(original)
+    end
+  end
+
+  describe "remaining guesses" do
+    it "returns number of guesses remaining for games" do
+      expect(game.remaining_guesses).to eq(5)
+      expect(losing_game.remaining_guesses).to eq(1)
+    end
+  end
+
+  describe "has_won and has_lost methods" do
+    it "returns false for a game in progress" do
+      expect(game.has_won?).to be(false)
+      expect(game.has_lost?).to be(false)
+    end
+  end
+
+  describe "incorrect guess" do
+    it "returns feedback for wrong guess" do
+      expect(game.guess(2)).to eq("Too low!")
+      expect(game.guess(11)).to eq("Too high!")
+    end
+    it "deducts from remaining guesses" do
+      game.guess(2)
+      expect(game.remaining_guesses).to eq(4)
+    end
+  end
+
+  describe "repeated guess" do
+    it "doesn't cost the user anything" do
+      game.guess(2)
+      game.guess(2)
+      expect(game.remaining_guesses).to eq(4)
+    end
+  end
+
+  describe "one guess remaining" do
+    it "gives too high warning when only one guess remaining" do
+      game.guess(4)
+      game.guess(3)
+      game.guess(2)
+      expect(game.guess(111)).to eq("Too high! WARNING: Only one guess left!")
+    end
+    it "gives too low warning when only one guess remaining" do
+      game.guess(4)
+      game.guess(3)
+      game.guess(2)
+      expect(game.guess(1)).to eq("Too low! WARNING: Only one guess left!")
+    end
+  end
+
+  describe "winning / losing game" do
+    it "gives correct feedback for winning game" do
+      expect(game.guess(8)).to eq("Correct! The number was 8")
+    end
+    it "gives correct feedback for losing game" do
+      expect(losing_game.guess(9)).to eq("You lost! The number was 999")
+    end
+  end
+
+  describe "finished game" do
+    it "guesses don't alter remaining_guesses" do
+      losing_game.guess(9)
+      losing_game.guess(8)
+      expect(losing_game.remaining_guesses).to eq(0)
+    end
+  end
+
+  describe "has_won? / has_lost?" do
+    it "reports outcome of winning game accurately" do
+      game.guess(8)
+      expect(game.has_won?).to be(true)
+      expect(game.has_lost?).to be(false)
+    end
+    it "reports outcome of losing game accurately" do
+      losing_game.guess(123)
+      expect(losing_game.has_won?).to be(false)
+      expect(losing_game.has_lost?).to be(true)
+    end
+  end
+
+
+
+
 end
